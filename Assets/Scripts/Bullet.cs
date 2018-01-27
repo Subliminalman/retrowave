3c8301195ac515ae1644b4050b81567c333fe640
Subliminalman <ryan.leonski@gmail.com>
2018-01-27 01:24:39 -0700

    Going through networking tutorial

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	void OnCollisionEnter (Collision _col) {
+
+		var hit = _col.gameObject;
+		var health = hit.GetComponent<Health>();
+		if (health != null) {
+			health.TakeDamage(10);
+		}
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Health : NetworkBehaviour {
+
+	public RectTransform healthBar;
+
+	public const int maxHealth = 100;
+
+	[SyncVar(hook = "OnChangeHealth")]
+	public int currentHealth = maxHealth;
+
+	public void TakeDamage (int _amount) {
+		if (!isServer) {
+			return;
+		}
+
+		currentHealth -= _amount;
+		if (currentHealth <= 0) {
+			currentHealth = 0;
+			RpcRespawn ();
+		}
+	}
+		
+	void OnChangeHealth (int currentHealth) {
+		healthBar.sizeDelta = new Vector2 ((((float)currentHealth) / maxHealth), healthBar.sizeDelta.y);
+	}
+
+	[ClientRpc]
+	void RpcRespawn () {
+		if (isLocalPlayer) {
+			transform.position = Vector3.zero;
+			currentHealth = maxHealth;
+		}
+	}
+}
